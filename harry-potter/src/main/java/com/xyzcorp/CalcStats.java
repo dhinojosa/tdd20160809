1dd7544da80c48d6f2fb6c15270449e95119a937
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-08-09 14:17:18 -0700

    Adding files

---
+++ harry-potter/src/main/java/com/xyzcorp/CalcStats.java
@@ -0,0 +1,50 @@
+package com.xyzcorp;
+
+import java.util.function.BiFunction;
+
+public class CalcStats {
+
+    private static void checkItemArray(int[] items) {
+        if (items == null) throw new NullPointerException("Array is null");
+        if (items.length == 0)
+            throw new IllegalArgumentException("Array length is zero");
+    }
+
+    private static int findByFunction(int[] items,
+                                      BiFunction<Integer, Integer, Boolean>
+                                              biFunction) {
+        int result = items[0];
+        for (int i = 1; i < items.length; i++) {
+            int nextValue = items[i];
+            if (biFunction.apply(result, nextValue)) result = nextValue;
+        }
+        return result;
+    }
+
+    public static int getMinimum(int[] items) {
+        checkItemArray(items);
+        return findByFunction(items, (result, nextValue) -> nextValue < result);
+    }
+
+    public static int getMaximum(int[] items) {
+        checkItemArray(items);
+        int result = items[0];
+        for (int i = 1; i < items.length; i++) {
+            int nextValue = items[i];
+            if (nextValue > result) result = nextValue;
+        }
+        return result;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ harry-potter/src/test/java/com/xyzcorp/CalcStatsTest.java
@@ -0,0 +1,121 @@
+package com.xyzcorp;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+public class CalcStatsTest {
+
+    @Test
+    public void testMinimumValueWithArrayOfNoItems() {
+
+        try {
+            CalcStats.getMinimum(new int[]{});
+            fail("This line should not run");
+        } catch (IllegalArgumentException iae) {
+            assertEquals(iae.getMessage(), "Array length is zero");
+        }
+    }
+    
+    @Test
+    public void testMinimumValueWithArrayOfOneItem() {
+        int result = CalcStats.getMinimum(new int[]{4});
+        assertEquals(result, 4);
+    }
+
+
+
+    @Test
+    public void testMinimumThatArrayIsNotNull() {
+         try {
+        	 CalcStats.getMinimum(null);
+        	 fail("This line should not run");
+         } catch (NullPointerException npe) {
+             assertEquals(npe.getMessage(), "Array is null");        	 
+         }
+    }
+
+    @Test
+    public void testMinimumValueWithArrayOfTwoItems3And4() {
+        int result = CalcStats.getMinimum(new int[]{3,4});
+        assertEquals(result, 3);
+    }
+
+    @Test
+    public void testMinimumValueWithArrayOfTwoItems4And3(){
+        int result = CalcStats.getMinimum(new int[]{4,3});
+        assertEquals(result, 3);
+    }
+
+    @Test
+    public void testMinimumValueWithANegative() {
+    	int result = CalcStats.getMinimum(new int[]{10,-4,3});
+    	assertEquals(result, -4);
+    }
+
+    @Test
+    public void testMinimumValueWithTwosNegativesAndSameValue() {
+    	int result = CalcStats.getMinimum(new int[]{10,-4,3,-4});
+    	assertEquals(result, -4);
+    }
+
+    @Test
+    public void testMinimumValueWithManyNegatives() {
+    	int result = CalcStats.getMinimum(new int[]{-1000,-333,-1,4,-3,10,-10001});
+    	assertEquals(result, -10001);
+    }
+
+    
+    @Test
+    public void testMaximumValueWithArrayOfNoItems() {
+        try {
+            CalcStats.getMaximum(new int[]{});
+            fail("This line should not run");
+        } catch (IllegalArgumentException iae) {
+            assertEquals(iae.getMessage(), "Array length is zero");
+        }
+    }
+
+    @Test
+    public void testMaximumValueWithArrayOfOneItem() {
+        int result = CalcStats.getMaximum(new int[]{4});
+        assertEquals(result, 4);
+    }
+
+    @Test
+    public void testMaximumThatArrayIsNotNull() {
+        try {
+            CalcStats.getMaximum(null);
+            fail("This line should not run");
+        } catch (NullPointerException npe) {
+            assertEquals(npe.getMessage(), "Array is null");
+        }
+    }
+
+    @Test
+    public void testMaximumValueWithArrayOfTwoItems3And4() {
+        int result = CalcStats.getMaximum(new int[]{3,4});
+        assertEquals(result, 4);
+    }
+
+    @Test
+    public void testMaximumValueWithArrayOfTwoItems4And3(){
+        int result = CalcStats.getMaximum(new int[]{4,3});
+        assertEquals(result, 4);
+    }
+
+
+    @Test
+    public void testMaximumValueWithANegative() {
+        int result = CalcStats.getMaximum(new int[]{10,-4,3});
+        assertEquals(result, 10);
+    }
+}
+
+
+
+
+
+
+
